7df14886a47782a87a476555eb1904c521e9568c
mohamedaliazouz <mohamedaliazouz@96268492-123d-14ed-8381-36f6e9720695>
2011-10-09 13:08:01 +0000

    adding new changes on code

--- trunk/test.php
+++ trunk/test.php
@@ -9,10 +9,13 @@ include 'randomPicFetcher.php'; //for if you do not know what to fill in as pic 
 include 'Header.php';
 
 
+//{"code":200,"token":{"key":"c0e6e2d7-f9d8-c8f4-aedd-d6f1d8fceddc","secret":"be47a5f60216aa8d61021e5dc5a5278c"}}
 //{"code":200,"token":{"key":"c5c9d9e7-d4e9-c1df-afea-ceddd2f9e4d8","secret":"f987a6601d09553ceb1f0bbed5665eb2"}}
 //{"code":200,"token":{"key":"fad3e7d1-ffce-c5d4-a3ca-f8d3feccf5e3","secret":"17feefcf4555d82fc5f9b905c5d8fcae"}}
 $key = "fad3e7d1-ffce-c5d4-a3ca-f8d3feccf5e3"; //$_POST['key'];
 $secret = "17feefcf4555d82fc5f9b905c5d8fcae"; // $_POST['secret'];
+//$key = "c0e6e2d7-f9d8-c8f4-aedd-d6f1d8fceddc";//"fad3e7d1-ffce-c5d4-a3ca-f8d3feccf5e3"; //$_POST['key'];
+//$secret ="be47a5f60216aa8d61021e5dc5a5278c";// "17feefcf4555d82fc5f9b905c5d8fcae"; // $_POST['secret'];
 
 $accessToken = new OAuthConsumer($key, $secret);
 $os = new osapiREST($OAUTHKEY, $OAUTHSECRET, $lan, $userid, $dbData, $debug, $accessToken);
@@ -21,30 +24,36 @@ $code = 200;
 $response = array();
 $result;
 $functionName;
-for ($i = 1; $i < 7; $i++) {
+for ($i = 1; $i < 2; $i++) {
     switch ($i) {
         case 1: {// get user information
                 $result = $os->getViewer();
-                $functionName="getViewer()";
+                foreach ($result["profilevisitors"] as $i->$value) {
+                    $visitorid=&$result["profilevisitors"][$i-1]["visitorid"];
+                    $temp=$os->getFriendDetails($visitorid);
+                    $visitorid=$temp;
+                    //echo json_encode(). "<br><br><br><br><br>";
+                }
+                $functionName = "getViewer()";
                 $code = 200;
             }break;
         case 2: {// get user friend list
                 $result = $os->getAllViewerFriends();
-                $functionName="getAllViewerFriends()";
+                $functionName = "getAllViewerFriends()";
                 $code = 200;
             } break;
         case 3: {  // get user notification
                 $result = $os->getViewerFriendsActivities();
-                $functionName="getViewerFriendsActivities()";
+                $functionName = "getViewerFriendsActivities()";
                 $code = 200;
             }break;
         case 4: {  // upload photo
                 $result = $os->getViewerFoF();
-                $functionName="getViewerFoF()";
+                $functionName = "getViewerFoF()";
                 $code = 200;
             }break;
         case 5: { //get user friend details
-            echo "Display All friend Via <b>getFriendDetails</b> <br>";
+                echo "Display All friend Via <b>getFriendDetails</b> <br>";
                 $friend_id = '169212791';
                 $result = $os->getFriendDetails($friend_id);
                 $response['code'] = $code;
@@ -84,17 +93,17 @@ for ($i = 1; $i < 7; $i++) {
                 $friend_id = '170215236';
                 $result = $os->getFriendDetails($friend_id);
                 $code = 200;
-                $functionName="getFriendDetails()";
+                $functionName = "getFriendDetails()";
             }break;
         case 6: { //get user friend details
-            $result = $os->getViewerFriends();
-            $functionName="getViewerFriends()";
+                $result = $os->getViewerFriends();
+                $functionName = "getViewerFriends()";
                 $code = 200;
             }break;
     }
     $response['code'] = $code;
     $response['result'] = $result;
-    echo "<b>".$functionName." </b><br>" . json_encode($response);
+    echo "<b>" . $functionName . " </b><br>" . json_encode($response);
     echo "<br><br><br><br>";
 }
 ?>
